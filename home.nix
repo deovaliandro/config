11060477c3ff24b74c0dff39674effb391af695c
deovaliandro <valiandrod@gmail.com>
2023-07-16 07:45:27 +0800

    update

--- home.nix
+++ home.nix
@@ -45,32 +45,32 @@
       withPython3 = true;
 
       plugins = with pkgs.vimPlugins; [
-        nvim-surround
-        nvim-treesitter
-        coc-nvim
-
+        nvim-autopairs
         vim-airline
+        vim-autoformat
+        coc-nvim
+        vim-devicons
         {
           plugin = vim-airline-themes;
           config = ''
-            let g:airline_theme='ayu_dark'
+            let g:airline_powerline_fonts = 1
+            set t_Co=256
             let g:airline#extensions#tabline#enabled = 1
             let g:airline#extensions#tabline#fnamemod = ':t'
-	    let g:airline#extensions#branch#enable = 1
-	    let g:airline_powerline_fonts = 1
-	    let g:airline#extensions#whitespace#enabled = 0
+	        let g:airline#extensions#branch#enable = 1
+	        let g:airline#extensions#whitespace#enabled = 0
+	        let g:airline#extensions#tabline#buffer_idx_mode = 1
+	        nmap <C-Left> :bp<CR>
+	        nmap <C-Right> :bn<CR>
           '';
         }
-        vim-commentary
-        vim-fugitive
-        vim-gitgutter
-	{
-          plugin = vim-unimpaired;
-	  config = ''
-	    nmap <C-k> [e
-	    nmap <C-j> ]e
-	  '';
-	}
+        {
+            plugin = vim-unimpaired;
+            config = ''
+	            nmap <C-k> [e
+	            nmap <C-j> ]e
+	        '';
+	    }
         {
           plugin = gruvbox-nvim;
           config = ''
@@ -94,10 +94,8 @@
 
       extraConfig = ''
         set number relativenumber
-        set cursorline
-        set scrolloff=5
-        set encoding=UTF-8
-	set nohlsearch
+	    set nohlsearch
+    	set expandtab ts=4 sw=4 ai
       '';
     };
 
@@ -139,19 +137,34 @@
       interactiveShellInit = ''
         set -gx GEM_HOME $HOME/gems
         set PATH $PATH $HOME/gems/bin
+        set -Ux TERM screen-256color-bce
+        set PATH $PATH $HOME/.local/bin
       '';
     };
 
     starship = {
       enable = true;
       enableFishIntegration = true;
+      enableTransience = true;
       settings = {
-        aws.style = "bold #ffb86c";
-        cmd_duration.style = "bold #f1fa8c";
-        directory.style = "bold #50fa7b";
-        hostname.style = "bold #ff5555";
+        aws = { disabled = true; };
+        gcloud = { disabled = true; };
+        
+        cmd_duration.style = "#f1fa8c";
+        directory.style = "#50fa7b";
+        hostname.style = "#ff5555";
+       
+        git_status = {
+            ahead = "⇡($count)";
+            diverged = "⇕⇡($ahead_count)⇣($behind_count)";
+            behind = "⇣($count)";
+            modified = "!($count)";
+            staged = "[++($count)](green)";
+        };
+
         git_branch.style = "bold #ff79c6";
         git_status.style = "bold #ff5555";
+
         username = {
           format = "[$user]($style) on ";
           style_user = "bold #bd93f9";
@@ -167,6 +180,26 @@
     zoxide = {
       enable = true;
       enableFishIntegration = true;
+    };
+
+    tmux = {
+      enable = true;
+      mouse = true;
+      shell = "${pkgs.fish}/bin/fish";
+      terminal = "tmux-256color";
+      historyLimit = 10000;
+      clock24 = true;
+      aggressiveResize = true;
+
+      plugins = with pkgs; [
+        {
+            plugin = tmuxPlugins.tmux-colors-solarized;
+            extraConfig = "set -g @colors-solarized '256'";
+        }
+      ];
+
+      extraConfig = ''
+      '';
     };
   };
 }
